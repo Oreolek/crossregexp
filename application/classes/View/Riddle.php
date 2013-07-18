8eb6e01dd38eacf355598ea2c642c91fc79fa827
Oreolek <keloero@oreolek.ru>
2013-07-18 13:12:35 +0700

    Trying out a hex grid

--- application/classes/View/Riddle.php
+++ application/classes/View/Riddle.php
@@ -4,6 +4,9 @@
  * Riddle view controller.
  **/
 class View_Riddle extends View_Layout {
+  public $scripts = array(
+    '//yandex.st/jquery/2.0.3/jquery.min.js',
+  );
   public function get_message()
   {
     return Markdown::instance()->transform($this->message);
